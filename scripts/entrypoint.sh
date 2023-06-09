46a091bc3c1dbc24e45dda769a918c2c6f7f9e28
Boris <bio-boris@github.com>
2023-06-09 12:58:12 -0500

    Update build

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ -n "$AUTODEPLOY" ]; then
+    echo "AUTODEPLOY environment variable is set. Performing Git clone..."
+    git clone https://github.com/kbase/static-content.git /app
+else
+    echo "AUTODEPLOY environment variable is not set. Skipping Git clone."
+fi
+
+exec /opt/bitnami/scripts/nginx/entrypoint.sh
+
